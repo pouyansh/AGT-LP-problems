a8da6c0a6749e412c4ccb096787301d41bc5635c
pouyansh <pshirzadian26@gmail.com>
2019-03-05 16:54:13 +0330

    judge -> mixed nash equilibrium finder

--- Judge.py
+++ Judge.py
@@ -26,11 +26,11 @@ def check_if_mixed_nash_equi(table, first_support, second_support):
     A_ub = []
     b_ub = []
 
-    # p_i + delta >= 0 , q_i + delta >= 0
+    # p_i - delta >= 0 , q_i - delta >= 0
     for i in range(len(first_support) + len(second_support)):
         temp_list = [0 for _ in range(len(first_support) + len(second_support) + 1)]
         temp_list[i] = 1
-        temp_list[len(temp_list)-1] = 1
+        temp_list[len(temp_list)-1] = -1
         A_ub.append(temp_list)
         b_ub.append(0)
 
@@ -42,6 +42,20 @@ def check_if_mixed_nash_equi(table, first_support, second_support):
 
     A_eq = []
     b_eq = []
+
+    # sigma p_i = 1 and sigma q_i = 0
+    temp_list = [0 for _ in range(len(first_support))]
+    for i in range(len(second_support)):
+        temp_list.append(1)
+    temp_list.append(0)
+    A_eq.append(temp_list)
+    b_eq.append(1)
+    temp_list = [1 for _ in range(len(first_support))]
+    for i in range(len(second_support)):
+        temp_list.append(0)
+    temp_list.append(0)
+    A_eq.append(temp_list)
+    b_eq.append(1)
 
     # set restricts for q_is in order to make it possible for the first person to plat mixed
     for i in range(1, len(first_support)):
@@ -58,18 +72,22 @@ def check_if_mixed_nash_equi(table, first_support, second_support):
         temp_list = []
         for j in range(len(first_support)):
             temp_list.append(
-                table[first_support[j]][second_support[i]][1] - table[first_support[j]][second_support[i]][1])
+                table[first_support[j]][second_support[i]][1] - table[first_support[j]][second_support[0]][1])
         for j in range(len(second_support)):
             temp_list.append(0)
         temp_list.append(0)
         A_eq.append(temp_list)
         b_eq.append(0)
 
-    res = linprog(c=c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq)
+    print(first_support, second_support, A_ub, b_ub, A_eq, b_eq)
+    if not A_ub == [] and not A_eq == []:
+        res = linprog(c=c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq)
+    elif not A_ub == []:
+        res = linprog(c=c, A_ub=A_ub, b_ub=b_ub)
     print(res.x)
 
 
-def mixed_nash_equi_finder_make_first_support(table, is_first, support):
+def mixed_nash_equi_finder_make_support(table, is_first, support):
     output_list = []
     if is_first:
         current_support = [0 for _ in range(len(table))]
@@ -84,7 +102,7 @@ def mixed_nash_equi_finder_make_first_support(table, is_first, support):
                     current_support[index] = 0
                     index += 1
                 current_support[index] = 1
-            output_list.append(mixed_nash_equi_finder_make_first_support(table, False, current_support))
+            output_list.append(mixed_nash_equi_finder_make_support(table, False, current_support))
     else:
         current_support = [0 for _ in range(len(table[0]))]
         temporary_variable = 1
@@ -98,11 +116,19 @@ def mixed_nash_equi_finder_make_first_support(table, is_first, support):
                     current_support[index] = 0
                     index += 1
                 current_support[index] = 1
-            check_if_mixed_nash_equi(table, support, current_support)
+            first_support = []
+            for i in range(len(support)):
+                if support[i] == 1:
+                    first_support.append(i)
+            second_support = []
+            for i in range(len(current_support)):
+                if current_support[i] == 1:
+                    second_support.append(i)
+            check_if_mixed_nash_equi(table, first_support, second_support)
 
 
 input_table = [[[3, 4], [2, 3], [3, 7]], [[4, 2], [5, 2], [5, 3]], [[6, 2], [4, 1], [2, 7]]]
 output = [[[3, 1]], [[0.5, 0, 0.5, 0.75, 0.25, 0]]]
 
 print(nash_equi_finder(input_table))
-mixed_nash_equi_finder_make_first_support(input_table, True, [])
+mixed_nash_equi_finder_make_support(input_table, True, [])
