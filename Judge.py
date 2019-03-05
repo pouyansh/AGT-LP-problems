2d943d4fd2035964cce411bf23ba78247bfc572d
pouyansh <pshirzadian26@gmail.com>
2019-03-05 14:57:06 +0330

    judge -> mixed nash equilibrium finder

--- Judge.py
+++ Judge.py
@@ -20,7 +20,89 @@ def nash_equi_finder(table):
     return output_list
 
 
+def check_if_mixed_nash_equi(table, first_support, second_support):
+    c = [0 for _ in range(len(first_support) + len(second_support))]
+    c.append(-1)
+    A_ub = []
+    b_ub = []
+
+    # p_i + delta >= 0 , q_i + delta >= 0
+    for i in range(len(first_support) + len(second_support)):
+        temp_list = [0 for _ in range(len(first_support) + len(second_support) + 1)]
+        temp_list[i] = 1
+        temp_list[len(temp_list)-1] = 1
+        A_ub.append(temp_list)
+        b_ub.append(0)
+
+    # delta >= 0
+    temp_list = [0 for _ in range(len(first_support) + len(second_support))]
+    temp_list.append(1)
+    A_ub.append(temp_list)
+    b_ub.append(0)
+
+    A_eq = []
+    b_eq = []
+
+    # set restricts for q_is in order to make it possible for the first person to plat mixed
+    for i in range(1, len(first_support)):
+        temp_list = [0 for _ in range(len(first_support))]
+        for j in range(len(second_support)):
+            temp_list.append(
+                table[first_support[i]][second_support[j]][0] - table[first_support[0]][second_support[j]][0])
+        temp_list.append(0)
+        A_eq.append(temp_list)
+        b_eq.append(0)
+
+    # set restricts for p_is in order to make it possible for the second person to plat mixed
+    for i in range(1, len(second_support)):
+        temp_list = []
+        for j in range(len(first_support)):
+            temp_list.append(
+                table[first_support[j]][second_support[i]][1] - table[first_support[j]][second_support[i]][1])
+        for j in range(len(second_support)):
+            temp_list.append(0)
+        temp_list.append(0)
+        A_eq.append(temp_list)
+        b_eq.append(0)
+
+    res = linprog(c=c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq)
+    print(res.x)
+
+
+def mixed_nash_equi_finder_make_first_support(table, is_first, support):
+    output_list = []
+    if is_first:
+        current_support = [0 for _ in range(len(table))]
+        temporary_variable = 1
+        while temporary_variable < 2 ** len(table):
+            temporary_variable += 1
+            if current_support[0] == 0:
+                current_support[0] = 1
+            else:
+                index = 0
+                while current_support[index] == 1:
+                    current_support[index] = 0
+                    index += 1
+                current_support[index] = 1
+            output_list.append(mixed_nash_equi_finder_make_first_support(table, False, current_support))
+    else:
+        current_support = [0 for _ in range(len(table[0]))]
+        temporary_variable = 1
+        while temporary_variable < 2 ** len(table[0]):
+            temporary_variable += 1
+            if current_support[0] == 0:
+                current_support[0] = 1
+            else:
+                index = 0
+                while current_support[index] == 1:
+                    current_support[index] = 0
+                    index += 1
+                current_support[index] = 1
+            check_if_mixed_nash_equi(table, support, current_support)
+
+
 input_table = [[[3, 4], [2, 3], [3, 7]], [[4, 2], [5, 2], [5, 3]], [[6, 2], [4, 1], [2, 7]]]
 output = [[[3, 1]], [[0.5, 0, 0.5, 0.75, 0.25, 0]]]
 
 print(nash_equi_finder(input_table))
+mixed_nash_equi_finder_make_first_support(input_table, True, [])
